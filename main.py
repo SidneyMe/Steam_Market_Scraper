a36f80fa2851d5e381bfe555ff0a17ac66b0d8f3
SidneyMe <147240957+SidneyMe@users.noreply.github.com>
2024-08-27 18:15:17 +0300

    chore: Refactor Steam class initialization and remove unused code

--- main.py
+++ main.py
@@ -21,23 +21,20 @@ def speed_test(func):
 
 class Steam:
 
- 
-    def __init__(self, urls, num_drivers=5) -> None:
+
+    def __init__(self, urls) -> None:
         self.steam_urls = urls
         self.timer_delay_time = 0
         self.items_list = []
         self.folio_urls = []
-        self.drivers = []
         chrome_options = Options()
         chrome_options.add_argument('--headless')
         chrome_options.add_argument('--disable-gpu')
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
         path_to_chromedriver = r'chromedriver/chromedriver.exe'
-        for _ in range(num_drivers):
-            service = Service(executable_path=path_to_chromedriver)
-            driver = webdriver.Chrome(service=service, options=chrome_options)
-            self.drivers.append(driver)
+        service = Service(executable_path=path_to_chromedriver)
+        self.driver = webdriver.Chrome(service=service, options=chrome_options)
 
 
     def url_processor(self):
@@ -48,16 +45,16 @@ class Steam:
     def sleep(self, sec):
         time.sleep(sec)
 
-    
-    def get_page(self, current_page, driver):        
-        driver.get(current_page)
+
+    def get_page(self, current_page):        
+        self.driver.get(current_page)
         self.sleep(self.timer_delay_time)
-        page = etree.HTML(driver.page_source)
+        page = etree.HTML(self.driver.page_source)
         return page
 
 
     def get_num_pages(self, url):
-        page = self.get_page(url, self.drivers[0])
+        page = self.get_page(url)
         num_items = int(page.xpath('//*[@id="searchResults_total"]')[0].text.replace(',', ''))
         num_of_pages = (num_items // 10) + (1 if num_items % 10 != 0 else 0)
         return num_of_pages + 1
@@ -66,16 +63,16 @@ class Steam:
     def steam_page_loader(self, current_url, current_page):
         page_loaded = False
         while not page_loaded:
-            page = self.get_page(current_url, self.drivers[0])
+            page = self.get_page(current_url)
             error_element = page.xpath('//*[@id="searchResultsRows"]/div/text()')[0]
             element = page.xpath('//*[@id="searchResultsRows"]')[0]
             opacity = element.attrib.get("style", "").split(";")[0].split(":")[1].strip()
             if 'error' in error_element:
-                self.drivers[0].refresh()
+                self.driver.refresh()
                 self.sleep(5)
                 print(f'Failed to load the page {current_page}, refreshing')
             elif opacity == '0.5':
-                self.drivers[0].refresh()
+                self.driver.refresh()
                 self.sleep(5)
                 print(f'Failed to load the page {current_page}, refreshing')
             else:
@@ -104,49 +101,22 @@ class Steam:
                     self.folio_urls.append(href.replace('https://steamcommunity.com/market/listings/730/', 'https://steamfolio.com/Item?name='))
 
 
-    def folio_page_loader(self, current_url, driver):
+    def folio_page_loader(self, current_url):
         page_loaded = False
         while not page_loaded:
             try:
-                page = self.get_page(current_url, driver)
+                page = self.get_page(current_url)
                 page.xpath('//*[@id="item-container"]/div/div/div/div[2]/div[2]/div/div[2]/div[1]/div/div/table/tbody/tr[2]/td/text()')[0]
             except IndexError:
-                driver.refresh()
+                self.driver.refresh()
                 self.sleep(5)
             else:
                 page_loaded = True
         return page
-    
-    
-    # def get_sales(self, url):
-    #     page = self.folio_page_loader(url)
-    #     sales_w = page.xpath('//*[@id="item-container"]/div/div/div/div[2]/div[2]/div/div[2]/div[1]/div/div/table/tbody/tr[2]/td/text()')[0]
-    #     sales_m = page.xpath('//*[@id="item-container"]/div/div/div/div[2]/div[2]/div/div[2]/div[1]/div/div/table/tbody/tr[3]/td/text()')[0]
-    #     sales_y = page.xpath('//*[@id="item-container"]/div/div/div/div[2]/div[2]/div/div[2]/div[1]/div/div/table/tbody/tr[4]/td/text()')[0]
-    #     return {
-    #                 'sales_w' : sales_w,
-    #                 'sales_m' : sales_m,
-    #                 'sales_y' : sales_y,         
-    #             }
 
-    # @speed_test
-    # def feature_to_urls(self):
-    #     self.timer_delay_time = 1
-    #     with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
-    #         feature_sales = list(executor.map(self.get_sales, self.folio_urls))
-            # for i in range(len(self.items_list)):
-            #     self.items_list[i].update(feature_sales[i])
 
-    def chuncify(self):
-        chunks = []
-        prev = 0
-        for url in range(0, len(self.folio_urls), 5):
-            chunks.append(self.folio_urls[prev:url])
-            prev = url
-        return chunks
-        
-    def get_sales(self, url, driver):
-        page = self.folio_page_loader(url, driver)
+    def get_sales(self, url):
+        page = self.folio_page_loader(url)
         sales_w = page.xpath('//*[@id="item-container"]/div/div/div/div[2]/div[2]/div/div[2]/div[1]/div/div/table/tbody/tr[2]/td/text()')[0]
         sales_m = page.xpath('//*[@id="item-container"]/div/div/div/div[2]/div[2]/div/div[2]/div[1]/div/div/table/tbody/tr[3]/td/text()')[0]
         sales_y = page.xpath('//*[@id="item-container"]/div/div/div/div[2]/div[2]/div/div[2]/div[1]/div/div/table/tbody/tr[4]/td/text()')[0]
@@ -159,12 +129,10 @@ class Steam:
     @speed_test
     def feature_to_urls(self):
         self.timer_delay_time = 1
-        chunks = self.chuncify()
-        feature_sales = []
-        for urls in chunks:
-            with concurrent.futures.ThreadPoolExecutor(max_workers=1) as executor:
-                feature_sales.append(list(executor.map(self.get_sales, urls, self.drivers)))
-        print(feature_sales)
+        with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+            feature_sales = list(executor.map(self.get_sales, self.folio_urls))
+            for i in range(len(self.items_list)):
+                self.items_list[i].update(feature_sales[i])
 
 
     def generate_xml(self):
@@ -189,14 +157,13 @@ class Steam:
 
     def close(self):
         try:
-            for driver in self.drivers:
-                driver.quit()
+            self.driver.quit()
         except Exception as ex:
             print(f'Failed to close webdriver {ex}')
 
 
 if __name__ == '__main__':
-    
+
     steam_urls = ['https://steamcommunity.com/market/search?category_730_ItemSet%5B%5D=any&category_730_ProPlayer%5B%5D=any&category_730_StickerCapsule%5B%5D=any&category_730_Tournament%5B%5D=any&category_730_TournamentTeam%5B%5D=tag_Team106&category_730_Type%5B%5D=any&category_730_Weapon%5B%5D=any&category_730_StickerCategory%5B%5D=tag_TeamLogo&category_730_StickerCategory%5B%5D=tag_Tournament&appid=730&q=#p1_price_asc']
     s = Steam(steam_urls)
     try:
