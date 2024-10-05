72f3a9ec8cb5e77adcfb0a5e4a26d0db85f7e174
SidneyMe <147240957+SidneyMe@users.noreply.github.com>
2024-10-05 18:58:56 +0300

    Refactor code to remove hardcoded URL and create an empty list for URLs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from scraper_manager import ScraperManager
 
 if __name__ == '__main__':
-    urls = ['https://steamcommunity.com/market/search?category_730_ItemSet%5B%5D=any&category_730_ProPlayer%5B%5D=any&category_730_StickerCapsule%5B%5D=any&category_730_Tournament%5B%5D=any&category_730_TournamentTeam%5B%5D=tag_Team106&category_730_Type%5B%5D=any&category_730_Weapon%5B%5D=any&category_730_StickerCategory%5B%5D=tag_TeamLogo&category_730_StickerCategory%5B%5D=tag_Tournament&appid=730&q=#p1_price_asc']    
+    urls = []    
     scraper_manager = ScraperManager(urls)
     scraper_manager.run()
