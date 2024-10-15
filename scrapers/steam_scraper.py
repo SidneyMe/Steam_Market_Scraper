b33c17379481299d395adb5af5c19b16162005cb
SidneyMe <147240957+SidneyMe@users.noreply.github.com>
2024-10-15 13:27:32 +0300

    Added comments. Improved scraping of num of pages.

--- scrapers/steam_scraper.py
+++ scrapers/steam_scraper.py
@@ -1,48 +1,42 @@
 import time
 from web_driver import WebDriver
 from scrapers.base_scraper import Scraper
+from lxml import etree
 
 
 class SteamScraper(Scraper):
     """
-    SteamScraper is a class that extends the Scraper class to scrape data from the Steam marketplace.
-    Attributes:
-        items_list (list): A list to store the scraped items.
-    Methods:
-        __init__(web_driver: WebDriver):
-            Initializes the SteamScraper with a web driver.
-        get_num_pages(url):
-            Calculates the number of pages to scrape based on the total number of items.
-        steam_page_loader(current_url):
-            Loads a Steam marketplace page and handles potential loading errors.
-        scrape(url):
-            Scrapes the Steam marketplace for items, iterating through all pages.
-        extract_item_info(market_listing):
-            Extracts item information from a market listing element.
+    Scrapes data from the Steam marketplace.
     """
-
 
     def __init__(self, web_driver: WebDriver):
         super().__init__(web_driver)
         self.items_list = []
 
 
-    def get_num_pages(self, url):
+    def get_num_pages(self, url: str) -> int:
+        """Calculates the number of pages to scrape based on the total number of items."""
+
         page = self.web_driver.get_page(url, 5)
-        num_items = int(page.xpath('//*[@id="searchResults_total"]')[0].text.replace(',', ''))
-        return (num_items // 10) + (1 if num_items % 10 != 0 else 0) + 1
+        num_items = int(page.xpath('//*[@id="searchResults_total"]')[0].text.replace(',', '')) #parses num of items and converts from str '20,000' to int
+        if num_items - 10 >= 0:
+            return num_items // 10 + 1
+        else:
+            return 2
 
+    def steam_page_loader(self, current_url: str) -> etree._Element:
+        """Loads a Steam marketplace page and handles potential loading errors."""
 
-    def steam_page_loader(self, current_url):
         while True:
             page = self.web_driver.get_page(current_url, 5)
-            error_element = page.xpath('//*[@id="searchResultsRows"]/div/text()')
+            error_element = page.xpath('//*[@id="searchResultsRows"]/div/text()')# looking for the "steam market search error"
             element = page.xpath('//*[@id="searchResultsRows"]')
-            if not error_element or not element:
+            if not error_element or not element: #if a steam search error or the page is blank
                 print('Failed to load the page, refreshing')
                 self.web_driver.driver.refresh()
                 time.sleep(5)
                 continue
+            #steam changes opacity of the steam items (items become blur) when it's trying to load a new page, sometimes it might stuck in that position.
             opacity = element[0].attrib.get("style", "").split(";")[0].split(":")[1].strip()
             if 'error' in error_element[0] or opacity == '0.5':
                 print('Failed to load the page, refreshing')
@@ -51,8 +45,9 @@ class SteamScraper(Scraper):
             else:
                 return page
 
+    def scrape(self, url: str):
+        """Scrapes the Steam marketplace for items, iterating through all pages."""
 
-    def scrape(self, url):
         steam_page_count = self.get_num_pages(url)
         base_url = url.split('#')[0]
         for current_page in range(1, steam_page_count):
@@ -61,8 +56,7 @@ class SteamScraper(Scraper):
             for market_listing in page.xpath('//*[@id="searchResultsRows"]/a[contains(@class, "market_listing_row")]'):
                 self.items_list.append(self.extract_item_info(market_listing))
 
-
-    def extract_item_info(self, market_listing):
+    def extract_item_info(self, market_listing: etree._Element) -> dict:
         return {
             'name': market_listing.xpath('.//div[contains(@class, "market_listing_item_name_block")]/span/text()')[0].replace('|', ''),
             'url': market_listing.get('href'),
