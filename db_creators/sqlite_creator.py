f3651e623f764f16bb2b619c5ba84f759a204d22
SidneyMe <147240957+SidneyMe@users.noreply.github.com>
2024-10-15 13:22:50 +0300

    Refactored DBCreator. Added a Migration class to update info about items that are already in the db.

--- db_creators/sqlite_creator.py
+++ db_creators/sqlite_creator.py
@@ -1,27 +1,13 @@
 import sqlite3
-import os
+from abc import ABC
 
-class SQLiteDatabaseCreator:
-    """
-    A class to create and manage an SQLite database for storing steam items.
-    Attributes:
-        DB_PATH (str): The path to the SQLite database file.
-        items_list (list): A list of dictionaries containing item data.
-        conn (sqlite3.Connection): The SQLite database connection object.
-        cur (sqlite3.Cursor): The SQLite database cursor object.
-    Methods:
-        __enter__(): Opens the database connection and returns the instance.
-        __exit__(exc_type, exc_val, exc_tb): Closes the database connection.
-        open_connection(): Opens the SQLite database connection.
-        close_connection(): Closes the SQLite database connection.
-        sql_scripts(): Returns the SQL script to create the steam_items table.
-        create_db(): Deletes the existing database, creates a new one, and inserts items.
-        delete_db(): Deletes the SQLite database file if it exists.
-        add_id(): Adds a unique ID to each item in the items_list.
-    """
+class SQLiteDB(ABC):
+
+    """Abstract method that creates connections with a db"""
+
     DB_PATH = 'output/steam_items.db'
 
-    def __init__(self, items_list):
+    def __init__(self, items_list: list[dict]):
         self.items_list = items_list
         self.conn = None
         self.cur = None
@@ -34,10 +20,12 @@ class SQLiteDatabaseCreator:
         self.close_connection()
 
     def open_connection(self):
+        """Creates connection the the db"""
         self.conn = sqlite3.connect(self.DB_PATH)
         self.cur = self.conn.cursor()
 
     def close_connection(self):
+        """Closes connection with the db if the connection was established"""
         if self.cur:
             self.cur.close()
         if self.conn:
@@ -45,7 +33,15 @@ class SQLiteDatabaseCreator:
         self.cur = None
         self.conn = None
 
-    def sql_scripts(self):
+
+class SQLiteDatabaseCreator(SQLiteDB):
+
+    """Creates a sqlite db and inserts items"""
+
+    def sql_script(self) -> str:
+
+        """Returns an sql scrip, which creates a table"""
+
         return """CREATE TABLE IF NOT EXISTS steam_items(
             id INTEGER PRIMARY KEY,
             name TEXT, url TEXT,
@@ -57,29 +53,61 @@ class SQLiteDatabaseCreator:
             )"""
 
     def create_db(self):
-        self.delete_db()
         self.add_id()
         self.open_connection()
         try:
-            self.cur.execute(self.sql_scripts())
-            self.cur.executemany("""INSERT INTO steam_items(
-                id, name, url, qty, price, sales_w, sales_m, sales_y
-                ) VALUES (:id, :name, :url, :qty, :price, :sales_w, :sales_m, :sales_y)""", self.items_list)
+            self.cur.execute(self.sql_script())
+            self.cur.execute("""INSERT INTO 
+                             steam_items(id, name, url, qty, price, sales_w, sales_m, sales_y) 
+                VALUES (:id, :name, :url, :qty, :price, :sales_w, :sales_m, :sales_y)
+                """, self.items_list)
             self.conn.commit()
         finally:
             self.close_connection()
 
-    def delete_db(self):
-        self.close_connection()
-        if os.path.exists(self.DB_PATH):
-            try:
-                os.remove(self.DB_PATH)
-                print(f"{self.DB_PATH} has been deleted.")
-            except PermissionError:
-                print(f"Unable to delete {self.DB_PATH}. It may be in use by another process.")
-        else:
-            print(f"{self.DB_PATH} does not exist.")
-
     def add_id(self):
+
+        """Adds an id to each item in the list"""
+
         for i, item in enumerate(self.items_list):
             item['id'] = i
+
+
+class SqliteMigration(SQLiteDB):
+
+    """Allows to update generated db"""
+
+    def make_migration(self):
+        
+        """Updates the existing db. Overwrites old data"""
+        
+        self.open_connection()
+        try:
+            for item in self.items_list:
+                db_item = self.cur.execute('SELECT * FROM steam_items WHERE name = ?', (item['name'],)).fetchone() #gets an item from the db with the same name of an item
+                if not db_item:
+                    self.add_new_items(item)
+                else:
+                    db_item_dict = {
+                        'qty': db_item[3],
+                        'price': db_item[4],
+                        'sales_w': db_item[5]
+                    }
+                    if item['qty'] != db_item_dict['qty'] or item['price'] != db_item_dict['price'] or item['sales_w'] != db_item_dict['sales_w']:
+                        self.cur.execute("""UPDATE steam_items
+                                            SET qty = ?, price = ?, sales_w = ?, sales_m = ?, sales_y = ?
+                                            WHERE name = ?
+                                        """, (item['qty'], item['price'], item['sales_w'], item['sales_m'], item['sales_y'], item['name']))
+            self.conn.commit()
+        finally:
+            self.close_connection()
+
+    def add_new_items(self, new_item: dict):
+        """Inserts a new item into the db"""
+
+        last_item = self.cur.execute('SELECT MAX(id) FROM steam_items').fetchone()[0]
+        new_item['id'] = int(last_item) + 1
+        self.cur.execute("""INSERT INTO steam_items
+                (id, name, url, qty, price, sales_w, sales_m, sales_y) 
+                VALUES (:id, :name, :url, :qty, :price, :sales_w, :sales_m, :sales_y)""", new_item)
+        
