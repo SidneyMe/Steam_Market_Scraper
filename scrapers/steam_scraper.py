8489aabe96583b42ca35bf5d7e05caf147e67526
SidneyMe <147240957+SidneyMe@users.noreply.github.com>
2025-01-07 11:45:34 +0200

    Refactor SteamScraper to remove inheritance from Scraper, implement improved page loading with Selenium, and streamline item data extraction

--- scrapers/steam_scraper.py
+++ scrapers/steam_scraper.py
@@ -1,68 +1,96 @@
 import time
-from scrapers.base_scraper import Scraper
-from lxml import etree
-from web_driver import WebDriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from selenium import webdriver
 
 
-class SteamScraper(Scraper):
+class SteamScraper:
     """
     Scrapes data from the Steam marketplace.
     """
 
-    def __init__(self, web_driver: WebDriver, urls: list[str]):
-        super().__init__(web_driver)
+    LOCATORS = {
+                #xpath
+                'items_num': '//*[@id="searchResults_total"]',
+                'ban': '//*[@id="mainContents"]/h2',
+                #class
+                'item_name': 'market_listing_item_name',
+                'qty': 'market_listing_num_listings_qty',
+                'price': 'normal_price',
+                'href': 'market_listing_row_link',
+                'active_table': 'market_content_block market_home_listing_table market_home_main_listing_table market_listing_table market_listing_table_active',
+                }
+
+
+    def __init__(self, web_drivers: list[webdriver.Chrome], urls: list[str]):
+        self.web_driver = web_drivers[0]
         self.urls = urls
+        self.wait = WebDriverWait
 
 
-    def get_num_pages(self, url: str) -> int:
-        """Calculates the number of pages to scrape based on the total number of items."""
-
-        page = self.web_driver.get_page(url, 5)
-        num_items = int(page.xpath('//*[@id="searchResults_total"]')[0].text.replace(',', '')) #parses num of items and converts from str '20,000' to int
-        if num_items - 10 >= 0:
-            return num_items // 10 + 1
-        else:
-            return 2
-
-    def steam_page_loader(self, current_url: str) -> etree._Element:
+    def page_loader(self, current_url: str) -> bool:
         """Loads a Steam marketplace page and handles potential loading errors."""
 
+        wait = self.wait(self.web_driver, 5)
+
         while True:
-            page = self.web_driver.get_page(current_url, 5)
-            error_element = page.xpath('//*[@id="searchResultsRows"]/div/text()')# looking for the "steam market search error"
-            element = page.xpath('//*[@id="searchResultsRows"]')
-            if not error_element or not element: #if a steam search error or the page is blank
-                print('Failed to load the page, refreshing')
-                self.web_driver.driver.refresh()
-                time.sleep(5)
-                continue
-            #steam changes opacity of the steam items (items become blur) when it's trying to load a new page, sometimes it might stuck in that position.
-            opacity = element[0].attrib.get("style", "").split(";")[0].split(":")[1].strip()
-            if 'error' in error_element[0] or opacity == '0.5':
-                print('Failed to load the page, refreshing')
-                self.web_driver.driver.refresh()
-                time.sleep(5)
-            else:
-                return page
+            self.web_driver.get(current_url)
+            time.sleep(3)
+
+            try:
+                if wait.until(EC.text_to_be_present_in_element((By.XPATH, self.LOCATORS['ban']), text_='Error')):
+                    print("You've been banned")
+                    time.sleep(300) #steam banns for 5min
+
+                if not wait.until(EC.visibility_of_element_located((By.CLASS_NAME, self.LOCATORS['active_table']))):
+                    self.web_driver.refresh()
+                    time.sleep(5) 
+           
+            except TimeoutException:
+                break
+
+        return True
+
+
+    def get_num_pages(self, url: str) -> dict[str, int]:
+        """Scrapes number of items and calculates number of pages"""
+
+        if not self.page_loader(url):
+            self.get_num_pages(url)
+
+        #gets num of items and converts from str '20,000' to int
+        num_items = int(self.wait(self.web_driver, 3).until(EC.visibility_of_element_located((By.XPATH, self.LOCATORS['items_num']))).text.replace(',', ''))
+
+        if num_items - 10 >= 0:
+            return {'num_pages': num_items // 10 + 1,
+                    'num_items': num_items} 
+        else:
+            return {'num_pages': 2,
+                    'num_items': num_items}
+
 
     def scrape(self) -> list[dict]:
         """Scrapes the Steam marketplace for items, iterating through all pages."""
-        
         steam_lots = []
         for url in self.urls:
-            steam_page_count = self.get_num_pages(url)
+            steam_page_count, item_num = self.get_num_pages(url).values()
             base_url = url.split('#')[0]
-            for current_page in range(1, steam_page_count):
-                current_url = f'{base_url}#p{current_page}_price_asc'
-                page = self.steam_page_loader(current_url)
-                for market_listing in page.xpath('//*[@id="searchResultsRows"]/a[contains(@class, "market_listing_row")]'):
-                    steam_lots.append(self.extract_item_info(market_listing))
-            return steam_lots
 
-    def extract_item_info(self, market_listing: etree._Element) -> dict:
-        return {
-            'name': market_listing.xpath('.//div[contains(@class, "market_listing_item_name_block")]/span/text()')[0].replace('|', ''),
-            'url': market_listing.get('href'),
-            'qty': market_listing.xpath('.//span[@class="market_listing_num_listings_qty"]/@data-qty')[0],
-            'price': market_listing.xpath('//*[@id="result_0"]/div[1]/div[2]/span[1]/span[1]/text()')[0],
-        }
+            for current_page in range(1, steam_page_count + 1):
+                current_url = f'{base_url}#p{current_page}_price_asc'
+
+                if self.page_loader(current_url):
+                    steam_items = self.web_driver.find_elements(By.CLASS_NAME, self.LOCATORS['href'])
+
+                    for steam_item in steam_items:
+                        splitted = steam_item.text.splitlines()
+                        steam_lots.append({
+                            'name': splitted[3],
+                            'qty': splitted[0],
+                            'price': splitted [2],
+                            'href': steam_item.get_attribute('href')
+                        })
+
+        return steam_lots
