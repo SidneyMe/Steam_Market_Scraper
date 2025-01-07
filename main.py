a2e1a31dcb224d00dc14f70b8235383ebb0e2100
SidneyMe <147240957+SidneyMe@users.noreply.github.com>
2025-01-07 11:45:45 +0200

    Update ScraperManager instantiation to include driver_num parameter for improved concurrency

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from scraper_manager import ScraperManager
 
 if __name__ == '__main__':
     urls = []
-    scraper_manager = ScraperManager(urls)
+    scraper_manager = ScraperManager(urls, driver_num=5)
     scraper_manager.run()
