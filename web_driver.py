abdf9fb31f4f76a7a8d9a506ed08cb1510f89423
SidneyMe <147240957+SidneyMe@users.noreply.github.com>
2025-01-07 11:44:51 +0200

    Refactor WebDriver class to implement singleton pattern and improve driver management

--- web_driver.py
+++ web_driver.py
@@ -1,43 +1,50 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
-from lxml import etree
 
 class WebDriver:
     """
     Manages the Chrome WebDriver.
     """
 
-    def __init__(self, urls = None):
-        self.urls = urls
+    _instance = None
+
+    def __new__(cls, drivers_num: int):
+        if cls._instance is None:
+            cls._instance = super(WebDriver, cls).__new__(cls)
+            cls._instance.init_webdrivers(drivers_num)
+        return cls._instance
+
+    def __init__(self, drivers_num: int):
+        if not hasattr(self, 'web_drivers'):
+            self.init_webdrivers(drivers_num)
+
+    def init_webdrivers(self, drivers_num: int):
+        self.wait = WebDriverWait
         chrome_options = Options()
         chrome_options.add_argument('--headless')
         chrome_options.add_argument('--disable-gpu')
         chrome_options.add_argument('--no-sandbox')
-        chrome_options.add_argument('--disable-dev-shm-usage') 
+        chrome_options.add_argument('--disable-dev-shm-usage')
         try:
             service = Service(ChromeDriverManager().install())
-            self.drivers = [webdriver.Chrome(service=service) for _ in len(self.urls)]
+            self.web_drivers = [webdriver.Chrome(service=service, options=chrome_options) for _ in range(drivers_num)]
         except Exception as e:
             print(f"Error initializing WebDriver: {e}")
             raise
 
-    def get_page(self, url: str, delay: int = 1) -> etree._Element:
-        """Loads a web page and returns its HTML content"""
-
+    def get_webdriver(self, req_drivers) -> list[webdriver.Chrome]:
         try:
-            for driver in self.drivers:
-                driver.get(url)
-                driver.implicitly_wait(delay)
-                return etree.HTML(driver.page_source)
-        except Exception as e:
-            print(f"Error loading page {url}: {e}")
-            return None
+            return self.web_drivers[0:req_drivers]
+        except IndexError:
+            print('Tried to get more drivers than were created')
+        return []
 
     def close(self):
         try:
-            for driver in self.drivers:
+            for driver in self.web_drivers:
                 driver.close()
         except Exception as ex:
             print(f'Failed closing the driver: {ex}')
